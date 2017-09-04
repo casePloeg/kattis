2202dd93a2255556d3e88578f14003ad0d2cc109
Case Ploeg <caseploog@gmail.com>
2017-09-03 20:26:36 -0400

    Create Ahhh.cpp

---
+++ Ahhh.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+//Case Ploeg
+//09-01-2017
+using namespace std;
+
+int main(){
+    string x, y;
+    cin >> x >> y;
+
+    if(x.length() < y.length()){
+        cout << "no" << std::endl;
+    }else{
+        cout << "go" << std::endl;
+    }
+}
+
